25be9c5f31bd4250aaaebc24469424e51e6a3076
Mikhail Melnik <by.zumzoom@gmail.com>
2021-01-04 10:57:07 +0300

    print only updated in second distr

--- airdrop/gen_second_distr.py
+++ airdrop/gen_second_distr.py
@@ -2,7 +2,16 @@ from distribution import gen_second_distribution, print_distribution
 
 
 def main():
-    print_distribution(gen_second_distribution(), 'second_distribution.json')
+    initial_distr = gen_initial_distribution()
+    second_distr = gen_second_distribution()
+    updated_distr = {}
+
+    for u, t in second_distr.items():
+        extra_tokens = t - (initial_distr.get(u) or 0)
+        if extra_tokens >= 1e18:
+            updated_distr[u] = extra_tokens
+
+    print_distribution(updated_distr, 'second_distribution.json')
 
 
 if __name__ == '__main__':
